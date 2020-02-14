637e9b23966674f9c93efa19a7d7f983cf552ba9
IFFranciscoME <if.francisco.me@gmail.com>
2020-02-14 10:22:32 -0600

    Inicial

---
+++ Tarea2/IDI_II_Tarea2_JFME.py
@@ -0,0 +1,25 @@
+
+# --
+
+
+def f_newton_raphson(f, x0, e):
+    """
+    Funcion para implementar el metodo de aproximacion numerica newton-raphson
+
+    Parameters
+    ----------
+    f :
+    x0 :
+    e :
+
+    Returns
+    -------
+
+    Debugging
+    -------
+
+    """
+
+    final = f + x0 + e
+
+    return final
