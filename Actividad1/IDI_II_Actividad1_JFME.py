2e5bce3fdbeb541b4629625ebc36180d2d6b366c
IFFranciscoME <if.francisco.me@gmail.com>
2020-03-28 13:07:20 -0600

    tarea8-actividad1

--- Actividad1/IDI_II_Actividad1_JFME.py
+++ Actividad1/IDI_II_Actividad1_JFME.py
@@ -1,29 +1,49 @@
 
 # -- ------------------------------------------------------------------------------------ -- #
-# -- proyecto: IDI-II: Ejercicio con Perceptron Multicapa
-# -- codigo: IDI_II_Tarea8_JFME.py
+# -- proyecto: IDI-II: Ejercicio en Clase con Perceptron Multicapa
+# -- codigo: IDI_II_Actividad1_JFME.py
 # -- repositorio: https://github.com/IFFranciscoME/IDI_II_GIT
 # -- ------------------------------------------------------------------------------------ -- #
 
+
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 # Leer datos de entrada
-datos = pd.read_excel('tabla_para_probar.xlsx')
+datos_excel = pd.read_excel('PercMultAplicado.xlsx')
 
-# Separar datos de entrenamiento y datos de prueba
-datos_test = datos[(datos['d1'] == '?') | (datos['d2'] == '?')]
-datos_test.reset_index(inplace=True, drop=True)
-datos_train = datos[(datos['d1'] != '?') | (datos['d2'] != '?')]
-datos_train.reset_index(inplace=True, drop=True)
+# datos originales con normalizacion de variables
+datos = pd.DataFrame({'x1': datos_excel['Monto']/300000,
+                      'x2': datos_excel['Mensualidad']/datos_excel['Ingreso mensual'],
+                      'x3': datos_excel['Antigüedad laboral (meses)']/180,
+                      'salida': datos_excel['Mora']})
 
-# -- Entrenamiento
-datos_x_train = np.array(datos_train.iloc[:, 0:-2], dtype=int)
-datos_d_train = np.array(datos_train.iloc[:, -2:], dtype=int)
+datos['salida'] = [1 if datos['salida'][i] == 'SI' else 0
+                   for i in range(0, len(datos['salida']))]
+
+datos = np.array(datos)
+datos_test, datos_train = train_test_split(datos, test_size=0.7, shuffle=False)
+
+# --------------------------------------------------------- PERCEPTRON MULTICAPA APLICADO -- #
+# --------------------------------------------------------------------------------------- -- #
+
+datos_x_train = datos_train[:, 0:-1]
+datos_d_train = datos_train[:, -1:]
 
 # -- PERCEPTRON MULTICAPA
-# [Entradas, Neuronas Ocultas, Salidas, Alfa]
-params = [4, 8, 1, 2.5]
+# [Entradas, Neuronas Ocultas, Salidas, Alfa, Error]
+params = [3, 10, 1, 0.85, 1e-15]
+
+# params = [3, 10, 1, 0.65, 1e-12] 0.81
+# params = [3, 10, 1, 0.85, 1e-12] 0.8157
+# params = [3, 10, 1, 0.85, 1e-14] 0.8142
+
+# params = [3, 8, 1, 0.65, 1e-13] 0.81
+# params = [3, 8, 1, 0.70, 1e-13] 0.8145
+# params = [3, 8, 1, 0.75, 1e-13] 0.8185
+# params = [3, 8, 1, 0.95, 1e-13] 0.81
 
 
 # ------------------------------------------------------------------------------- Proceso -- #
@@ -63,12 +83,14 @@ w_o_ini = w_o
 # -- ------------------------------------------------------- ITERACIONES DE ENTRENAMIENTO -- #
 
 error = 1
+salidas = list()
 # entrenar hasta lograr pasar un error objetivo
-while error > 1e-6:
+while error > params[4]:
+    print(' ------------ CICLO WHILE ------------ ')
     # ciclo para las q observaciones de entrenamiento
     for q in range(len(datos_x_train)):
         # q = 0
-        print('iteracion (Q): ' + str(q))
+        # print(' **** iteracion (Q): ' + str(q) + ' **** ')
         # vector Q de N entradas
         x_j = datos_x_train[q, :][np.newaxis, :]
 
@@ -81,8 +103,7 @@ while error > 1e-6:
         net_o = w_o.dot(y_h)
         # salida
         y = funcion_activacion(param_f='sigmoid', param_x=net_o, param_alfa=params[3])
-        print(y.T)
-        # print('d1 = ' + str(round(y[0][0], 2)) + ' | d2 = ' + str(round(y[1][0], 2)))
+        salidas.append(y)
 
         # -- BACKWARD
         # errores de la capa de salida
@@ -106,34 +127,13 @@ while error > 1e-6:
 
 print(' ----- ENTRENAMIENTO terminado -----')
 
-# print('Termino entrenamiento')
-# print('los pesos en capa oculta quedaron: ')
-# print(w_h)
-# print('los pesos en capa de salida quedaron: ')
-# print(w_o)
+salidas_y_hat = [1 if salidas[i][0][0] >= 0.50 else 0 for i in range(0, len(datos_d_train))]
+salidas_y = [int(datos_d_train[i][0]) for i in range(0, len(datos_d_train))]
+comparacion = pd.DataFrame({'y': salidas_y, 'y_hat': salidas_y_hat})
+cm = confusion_matrix(salidas_y, salidas_y_hat)
 
-# -- ------------------------------------------------------------- PREDICCION CON PRUEBAS -- #
+# [ 1-1, 1-0]
+# [ 0-1, 0-0]
 
-# -- volver a validar con datos de entrenamiento
-datos_x_train = np.array(datos_train.iloc[:, 0:-2], dtype=int)
-
-# -- Prueba
-datos_x_test = np.array(datos_test.iloc[:, 0:-2], dtype=int)
-
-print(' ----- PRUEBA iniciada -----')
-
-for q in range(len(datos_x_test)):
-    # vector Q de N entradas
-    x_j = datos_x_test[q, :][np.newaxis, :]
-
-    # -- FORWARD
-    # red de la capa oculta
-    net_h = w_h.dot(x_j.T)
-    # salidas de capa oculta
-    y_h = funcion_activacion(param_f='sigmoid', param_x=net_h, param_alfa=params[3])
-    # red de capa de salida
-    net_o = w_o.dot(y_h)
-    # salida
-    y = funcion_activacion(param_f='sigmoid', param_x=net_o, param_alfa=params[3])
-    print(y.T)
-    # print('d1 = ' + str(round(y[0][0], 2)) + ' | d2 = ' + str(round(y[1][0], 2)))
+aciertos = (cm[0][0] + cm[1][1])/len(datos_d_train)
+print(aciertos)
