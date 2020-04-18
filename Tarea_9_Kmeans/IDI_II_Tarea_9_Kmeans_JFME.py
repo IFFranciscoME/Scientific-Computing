e0578c4c4ed9d16df0ab48dbb97c01eb2cdfd868
IFFranciscoME <if.francisco.me@gmail.com>
2020-04-18 09:51:35 -0500

    cambio en estructura

---
+++ Tarea_9_Kmeans/IDI_II_Tarea_9_Kmeans_JFME.py
@@ -0,0 +1,16 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: IDI-II: Ejercicio con Kmeans
+# -- codigo: IDI_II_Tarea_9_Kmeans_JFME.py
+# -- repositorio: https://github.com/IFFranciscoME/IDI_II_GIT
+# -- ------------------------------------------------------------------------------------ -- #
+
+"""
+para instalar librerias necesarias para el proyecto ejecutar
+$ pip install -r requirements.tx
+"""
+
+import pandas as pd
+import numpy as np
+
+_
