2027feca34a4e3d48c7a061c1f0866c5ae4f9021
IFFranciscoME <if.francisco.me@gmail.com>
2020-05-06 10:15:47 -0500

    version final

--- Actividad_2_Kmeans_Cohonen/funciones.py
+++ Actividad_2_Kmeans_Cohonen/funciones.py
@@ -16,8 +16,8 @@ from PIL import Image as im
 # -- ------------------------------------------------------- FUNCION: Lectura de imagenes -- #
 # -- ------------------------------------------------------------------------------------ -- #
 def f_entrada_imagenes(param_nombre):
-
     """
+    Funcion para leer archivos de imagenes como datos de entrada
     Parameters
     ----------
     param_nombre : str : Nombre del archivo a leer
@@ -34,7 +34,7 @@ def f_entrada_imagenes(param_nombre):
     # obtener directorio de trabajo actual para encontrar ~/imagenes/archivo.jpg
     directorio = os.getcwd()
     # cargar archivo
-    archivo_imagen = im.open(directorio + '/imagenes/' + param_nombre)
+    archivo_imagen = im.open(directorio + '/imagenes/originales/' + param_nombre)
     # dejar como array a los datos obtenidos de la imagen
     imagen = np.array(archivo_imagen)
     # asegurarse que el array es escribible
@@ -43,24 +43,56 @@ def f_entrada_imagenes(param_nombre):
     total = archivo_imagen.size[0]*archivo_imagen.size[1]
     # reacomodar para tener todos los datos en una matriz vertical
     datos_imagen = np.reshape(imagen, (total, 3))
-    # crear un array de 0s y 4 columnas
-    # datos_imagen = np.zeros((total, 4))
-    # escribir los datos de los pixeles (3 componentes) y una 4 para apoyo posterior
-    # datos_imagen[:, :-1] = imagen
 
     return {'datos': datos_imagen, 'dimensiones': archivo_imagen.size}
 
 
-# -- --------------------------------------- FUNCION: Calculo de distancia entre vectpres -- #
+# -- ----------------------------------------------------------- FUNCION: Escribir imagen -- #
 # -- ------------------------------------------------------------------------------------ -- #
 
-def distancia(param_a, param_b):
+def f_reescribir_imagen(param_data, param_dims, param_nombre):
     """
+    Funcion para reescribir un array de datos RGB en una imagen
+
+    Parameters
+    ----------
+    param_data : np.array : datos de entrada en un array
+    param_dims : np.array : con dimensiones de la imagen de salida
+    param_nombre : str : nombre del archivo a escribir incluyendo extension
+
+    Returns
+    -------
+    1
+
+    Debugging
+    ---------
+    param_dims = imagen['dimensiones']
+    param_data = r_kmeans['datos']
+    param_nombre = 'kmeans_salida.jpg'
+
+    """
+
+    imagen = np.reshape(np.array(param_data)[:, 0:3], (param_dims[1], param_dims[0], 3))
+    datos = imagen.astype(np.uint8)
+    nueva_imagen = im.fromarray(datos)
+
+    directorio = os.getcwd()
+    nueva_imagen.save(directorio + '/imagenes/resultados/' + param_nombre)
+
+
+# -- --------------------------------------- FUNCION: Calculo de distancia entre vecopres -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+def distancia(param_a, param_b, param_d):
+    """
+    funcion para calcular una medida de distancia especificada por (param_dist) entre
+    dos vectores (param_a) y (param_b)
 
     Parameters
     ----------
     param_a : np.array : vector a
     param_b : np.array : vector b
+    param_d : str : 'euclid' para euclidiana
 
     Returns
     -------
@@ -74,9 +106,10 @@ def distancia(param_a, param_b):
     """
 
     # calculo de distancia euclideana
-    dist = np.linalg.norm(param_b - param_a)
-
-    return dist
+    if param_d == 'euclid':
+        return np.linalg.norm(param_b - param_a)
+    else:
+        print('esa medida de distancia no esta especificada')
 
 
 # -- --------------------------------------------------------- FUNCION: Metodo de Kohonen -- #
@@ -84,17 +117,21 @@ def distancia(param_a, param_b):
 
 def f_kohonen(param_data, param_k, param_t, param_p):
     """
+    Funcion que utiliza mapas autoasociativos (Red de Kohonen) para clasificar datos
+    de entrada (param_data) segun las clases deseadas (param_k), realizando una cantidad
+    de iteraciones definida por (param_t) y parando el proceso cuando se alcanza un
+    error definido por (param_p)
 
     Parameters
     ----------
-    param_data
-    param_k
-    param_t
-    param_p
+    param_data : np.array : array con N cantidad de columnas para N dimensiones
+    param_k : int : cantidad de clusters a utilizar para la clasificacion
+    param_t : int : cantidad de iteraciones
+    param_p : int : error como criterio de paro
 
     Returns
     -------
-
+    {'datos': datos, 'centroides': centroides}
 
     Debugging
     ---------
@@ -135,7 +172,7 @@ def f_kohonen(param_data, param_k, param_t, param_p):
     error = float("inf")
     # contador de iteraciones inicial
     iteracion = 0
-
+    # inicializar objeto de datos
     kohonen_data = pd.DataFrame()
 
     # -- iteracion para acomodar centroides
@@ -184,25 +221,15 @@ def f_kohonen(param_data, param_k, param_t, param_p):
             for i in range(len(nvo_centroide)):
                 centroides[cent_dato][i] = nvo_centroide[i]
 
-            # criterio para decimales, truncarlos a
-            # centroides = np.trunc(centroides)
-
             # calcular diferencia de errores
             error = np.nanmax(abs(centroides_o - centroides))
-
-            # print(error)
 
         # impresiones de control
         print('\n')
         print('iteracion: ', iteracion)
         print('error: ', error)
 
-    # mensaje de salida
-    print('el algoritmo convergio en: ' + str(iteracion) + ' iteraciones')
-
-    # kohonen_data
-    # segun la columna de cent, asignar coordenadas de ultimos centroides
-    # a cada dato
+    # kohonen_data segun la columna de cent, asignar coordenadas de ultimos centroides
     for i_centroide in range(len(centroides)):
         kohonen_data.loc[kohonen_data['cent'] == i_centroide,
                          ['dato_x', 'dato_y', 'dato_z']] = centroides[i_centroide]
@@ -215,14 +242,18 @@ def f_kohonen(param_data, param_k, param_t, param_p):
 
 # -- --------------------------------------------------------- FUNCION: Metodo de K-Means -- #
 # -- ------------------------------------------------------------------------------------ -- #
-def f_kmeans(param_data, param_k, param_iter):
-
+def f_kmeans(param_data, param_k, param_t, param_p):
     """
+    Funcion que utiliza Kmeans para clasificar datos de entrada (param_data) segun
+    las clases deseadas (param_k), realizando una cantidad de iteraciones definida por
+    (param_t) y parando el proceso cuando se alcanza un error definido por (param_p)
+
     Parameters
     ----------
     param_data : np.array : array con N cantidad de columnas para N dimensiones
     param_k : int : cantidad de clusters a utilizar para la clasificacion
-    param_iter : int : cantidad de iteraciones
+    param_t : int : cantidad de iteraciones
+    param_p : int : error como criterio de paro
 
     Returns
     -------
@@ -261,9 +292,9 @@ def f_kmeans(param_data, param_k, param_iter):
 
     # banderas para while
     iteracion = 0
-    error = 2
+    error = float("inf")
 
-    while error > 1:
+    while error > param_p:
         iteracion += 1
         centroides_n = copy.deepcopy(centroides)
         v = 0
@@ -272,7 +303,8 @@ def f_kmeans(param_data, param_k, param_iter):
         for i_dato in datos:
             # i_dato = datos[0]
             distancias = []
-            [distancias.append(distancia(param_a=i_dato, param_b=m)) for m in centroides]
+            [distancias.append(distancia(param_a=i_dato, param_b=i_cent, param_d='euclid'))
+             for i_cent in centroides]
 
             # agregar informacion de centroide correspondiente al dato
             datos[v, n-1] = distancias.index(min(distancias))
@@ -295,34 +327,3 @@ def f_kmeans(param_data, param_k, param_iter):
     datos = [centroides[int(i_dato[n-1])] for i_dato in datos]
 
     return {'datos': datos, 'centroides': centroides}
-
-
-# -- --------------------------------------------------------- FUNCION: Metodo de K-Means -- #
-# -- ------------------------------------------------------------------------------------ -- #
-
-def f_reescribir_imagen(param_data, param_dims, param_nombre):
-
-    """
-    Parameters
-    ----------
-    param_data : np.array : datos de entrada en un array
-    param_dims : np.array : con dimensiones de la imagen de salida
-    param_nombre : str : nombre del archivo a escribir incluyendo extension
-
-    Returns
-    -------
-
-    Debugging
-    ---------
-    param_dims = imagen['dimensiones']
-    param_data = r_kmeans['datos']
-    param_nombre = 'kmeans_salida.jpg'
-
-    """
-
-    imagen = np.reshape(np.array(param_data)[:, 0:3], (param_dims[1], param_dims[0], 3))
-    datos = imagen.astype(np.uint8)
-    nueva_imagen = im.fromarray(datos)
-
-    directorio = os.getcwd()
-    nueva_imagen.save(directorio + '/imagenes/' + param_nombre)
--- Actividad_2_Kmeans_Cohonen/principal.py
+++ Actividad_2_Kmeans_Cohonen/principal.py
@@ -13,26 +13,46 @@ from os.path import isfile, join
 # Semilla para aleatorios
 np.random.seed(2020)
 
+# -------------------------------------------------------------- generar datos de entrada -- #
 # busca en el sistema el directorio donde este la carpeta
-abspath = path.abspath('imagenes/')
+abspath = path.abspath('imagenes/originales/')
 
 # extrae todos los nombres de los archivos dentro de esa carpeta
 archivos = [f for f in listdir(abspath) if isfile(join(abspath, f))]
-# ['bandera_alemania.jpg', 'bandera_mexico.jpg', 'paisaje_riscos.jpg', 'persona_gandhi.jpg']
 
+# numero de archivo
+# 0 -> 'bandera_alemania.jpg', 1 -> 'bandera_mexico.jpg',
+# 2 -> 'paisaje_riscos.jpg', 3 -> 'persona_gandhi.jpg'
+archivo = 3
 # obtener los datos de la imagen
-imagen = fn.f_entrada_imagenes(param_nombre=archivos[2])
+imagen = fn.f_entrada_imagenes(param_nombre=archivos[archivo])
+# K para correr pruebas
+k = 2
 
+# -- ---------------------------------------------------------------------------- K-MEANS -- #
 # obtener resultados con k-means
-# r_kmeans = fn.f_kmeans(param_data=imagen['datos'], param_k=3, param_iter=50)
+r_kmeans = fn.f_kmeans(param_data=imagen['datos'], param_k=k, param_t=50, param_p=1e-1)
 
 # reescribir la imagen
-# fn.f_reescribir_imagen(param_data=r_kmeans['datos'], param_dims=imagen['dimensiones'],
-#                        param_nombre='kmeans_salida.jpg')
+fn.f_reescribir_imagen(param_data=r_kmeans['datos'], param_dims=imagen['dimensiones'],
+                       param_nombre='/kmeans/' + archivos[archivo][0:-4] +
+                                    '_kmeans_i50_k' + str(k) + '.jpg')
 
+# Para K=20 en la imagen persona_gandhi.jpg
+# iteracion:  38
+# error:  0.07877499425418932
+# tiempo: 39 segundos aprox
+
+# -- ---------------------------------------------------------------------------- KOHONEN -- #
 # obtener resultados con Kohonen
-r_kohonen = fn.f_kohonen(param_data=imagen['datos'], param_k=24, param_t=10, param_p=1e1)
+r_kohonen = fn.f_kohonen(param_data=imagen['datos'], param_k=k, param_t=50, param_p=1e-1)
 
 # reescribir la imagen
 fn.f_reescribir_imagen(param_data=r_kohonen['datos'], param_dims=imagen['dimensiones'],
-                       param_nombre='kohonen_salida.jpg')
+                       param_nombre='/kohonen/' + archivos[archivo][0:-4] +
+                                    '_kohonen_i50_k' + str(k) + '.jpg')
+
+# Para K=20 en la imagen persona_gandhi.jpg
+# iteracion:  274
+# error:  0.09993658905139569
+# tiempo: 12 minutos aprox
