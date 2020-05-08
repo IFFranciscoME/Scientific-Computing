a3795378da1ec5a401c045be914ef7550f0cf7db
IFFranciscoME <if.francisco.me@gmail.com>
2020-05-08 18:01:07 -0500

    version funcional pre finalizada

--- Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
+++ Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
@@ -21,8 +21,10 @@ iteraciones que fueron necesarias para alcanzar la respuesta:
 
 import numpy as np
 import sympy as sp
+from sympy import symbols
+from sympy.plotting import plot3d
 
-x, y, z = sp.symbols('x y z')
+x, y, z = symbols('x y z')
 
 
 # -- ---------------------------------------------- FUNCION: Newton Raphson Multivariado -- #
@@ -58,29 +60,67 @@ def newton_raphson(param_sis, param_ini, param_error):
     valores = -mult.subs(list(zip(variables, param_ini))) + np_param_ini
     error = np.sum(-mult.subs(list(zip(variables, param_ini))))
 
+    iteraciones = 0
+
     while error > param_error:
 
+        iteraciones += 1
         valores = -mult.subs(list(zip(variables, valores)))
         valores_n = valores + np_param_ini
         error = np.sum(valores)
         valores = valores_n
 
-    return valores
+    valores = [round(valores[i], 4) for i in range(0, len(valores))]
+
+    return {'solucion': valores, 'iteraciones': iteraciones}
 
 
 # -- ----------------------------------------------------------------------------- Caso 1 -- #
-# c1_f = 'x**2 + y - 1'
-# c1_g = 'x - 2*y**2'
-# variables = [x, y]
-#
-# ejercicio_1 = newton_raphson(param_sis=[c1_f, c1_g],
-#                              param_ini=[0, 0],
-#                              param_error=1e-4)
-# print(ejercicio_1)
+
+# Sistema de ecuaciones
+c1_f = 'x**2 + y - 1'
+c1_g = 'x - 2*y**2'
+variables = [x, y]
+
+# plot expandido para vision general
+plot3d(c1_f, c1_g)
+
+# Buscar solucion al sistema
+ejercicio_1 = newton_raphson(param_sis=[c1_f, c1_g],
+                             param_ini=[0, 0],
+                             param_error=1e-3)
+
+# -- Resultados
+# Valores Iniciales:
+# [0, 0]
+
+# Itearciones:
+print(ejercicio_1['iteraciones'])
+
+# Solucion:
+print(ejercicio_1['solucion'])
+
+# plot focalizado a resultados
+plot3d(c1_f, c1_g, (x, -1, 0), (y, -3, -0.5))
 
 # -- ----------------------------------------------------------------------------- Caso 2 -- #
-# c2_f = 'x**2 - 10*x + y**2 + 5'
-# c2_g = 'x*y**2 + x - 10*y + 8'
+
+# ecuaciones
+c2_f = 'x**2 - 10*x + y**2 + 5'
+c2_g = 'x*y**2 + x - 10*y + 8'
+
+# plot expandido para vision general
+plot3d(c2_f, c2_g)
+
+# Buscar solucion al sistema
+ejercicio_2 = newton_raphson(param_sis=[c2_f, c2_g],
+                             param_ini=[0, 0],
+                             param_error=1e-14)
+# valores de variables
+print(ejercicio_2)
+
+# plot focalizado
+plot3d(c1_f, c1_g, (x, -1, 0), (y, -3, -0.5))
 
 # -- ----------------------------------------------------------------------------- Caso 3 -- #
 # c3_f = 'x + y - z + 2'
@@ -88,6 +128,7 @@ def newton_raphson(param_sis, param_ini, param_error):
 # c3_h = 'z - y**2 - 1'
 
 # -- ----------------------------------------------------------------------- Caso Ejemplo -- #
+
 c1_f = 'x**3 + y**3 - z**3 - 129'
 c1_g = 'x**2 + y**2 - z**2 - 9.75'
 c1_h = 'x + y - z - 9.49'
