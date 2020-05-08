5c54643dfaa69cb26ad105ed0486c12b460a2876
IFFranciscoME <if.francisco.me@gmail.com>
2020-05-08 12:52:17 -0500

    avance de base

--- Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
+++ Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
@@ -1,4 +1,3 @@
-
 # -- ------------------------------------------------------------------------------------ -- #
 # -- proyecto: IDI-II
 # -- codigo: IDI_II_Tarea3_JFME.py
@@ -27,13 +26,32 @@ x, y, z = sp.symbols('x y z')
 # -- ----------------------------------------------------------------------------- Caso 1 -- #
 c1_f = 'x**2 + y - 1'
 c1_g = 'x - 2*y**2'
+X = sp.MatrixSymbol('A', 2, 1)
 
 # -- ----------------------------------------------------------------------------- Caso 2 -- #
 c2_f = 'x**2 - 10*x + y**2 + 5'
 c2_g = 'x*y**2 + x - 10*y + 8'
+c2_A = sp.MatrixSymbol('A', 2, 1)
 
 # -- ----------------------------------------------------------------------------- Caso 3 -- #
 c3_f = 'x + y - z + 2'
 c3_g = 'x**2 + y'
 c3_h = 'z - y**2 - 1'
+c3_A = sp.MatrixSymbol('A', 3, 1)
 
+
+class NewtonRaphson:
+    """
+    Metodo de Newton-Raphson Multivariado
+    """
+    def __init__(self, funciones, variables):
+        # atributo de clase seran sus funciones
+        self.funciones = funciones
+        # guardar funciones en matriz para representarlas como sistema de ecuaciones
+        matriz_sistema = sp.Matrix(funciones)
+        # calcular la matriz jacobiana del sistema de funciones
+        self.jacobiana = matriz_sistema.jacobian(variables)
+        # obtener la inversa de la matriz jacobiana
+        self.jacobiana_inv = self.jacobiana.inv()
+        # operacion de multiplicacion de inversa de jacobiana por matriz del sistema
+        self.mult = self.jacobiana_inv * matriz_sistema
