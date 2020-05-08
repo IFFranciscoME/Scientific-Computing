e369d6869fdeafff2fe851a75c6ba47995695a54
IFFranciscoME <if.francisco.me@gmail.com>
2020-05-08 17:06:46 -0500

    version casi funcional

--- Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
+++ Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
@@ -18,24 +18,82 @@ En todos los casos indique el(los) valor(es) inicial(es) que utilizó y el núme
 iteraciones que fueron necesarias para alcanzar la respuesta:
 """
 
+import numpy as np
 import sympy as sp
 
-# declarar a x, y, z como variables simbolicas
 x, y, z = sp.symbols('x y z')
 
+
+# -- ---------------------------------------------- FUNCION: Newton Raphson Multivariado -- #
+
+def newton_raphson(param_sis, param_ini, param_error):
+    """
+
+    Parameters
+    ----------
+    param_sis : list : lista con objetos de funciones simbolicas
+    param_ini : list : Lista con valores iniciales para variables
+    param_error : float : 10e-3
+
+    Returns
+    ------
+    valor_5
+
+    Debugging
+    ---------
+    param_ini = [4, 2, -3]
+    param_sis = [c1_f, c1_g, c1_h]
+    param_error = 10e-2
+
+    """
+
+    np_param_ini = np.vstack(np.array(param_ini))
+    sistema = sp.Matrix(param_sis)
+    jacobian = sistema.jacobian(variables)
+    jacobian_inv = jacobian.inv()
+    mult = jacobian_inv * sistema
+
+    error = float("inf")
+    valores = -mult.subs(list(zip(variables, param_ini))) + np_param_ini
+    error = np.sum(-mult.subs(list(zip(variables, param_ini))))
+
+    while error > param_error:
+
+        valores = -mult.subs(list(zip(variables, valores)))
+        valores_n = valores + np_param_ini
+        error = np.sum(valores)
+        valores = valores_n
+
+    return valores
+
+
 # -- ----------------------------------------------------------------------------- Caso 1 -- #
-c1_f = 'x**2 + y - 1'
-c1_g = 'x - 2*y**2'
-X = sp.MatrixSymbol('A', 2, 1)
+# c1_f = 'x**2 + y - 1'
+# c1_g = 'x - 2*y**2'
+# variables = [x, y]
+#
+# ejercicio_1 = newton_raphson(param_sis=[c1_f, c1_g],
+#                              param_ini=[0, 0],
+#                              param_error=1e-4)
+# print(ejercicio_1)
 
 # -- ----------------------------------------------------------------------------- Caso 2 -- #
-c2_f = 'x**2 - 10*x + y**2 + 5'
-c2_g = 'x*y**2 + x - 10*y + 8'
-c2_A = sp.MatrixSymbol('A', 2, 1)
+# c2_f = 'x**2 - 10*x + y**2 + 5'
+# c2_g = 'x*y**2 + x - 10*y + 8'
 
 # -- ----------------------------------------------------------------------------- Caso 3 -- #
-c3_f = 'x + y - z + 2'
-c3_g = 'x**2 + y'
-c3_h = 'z - y**2 - 1'
-c3_A = sp.MatrixSymbol('A', 3, 1)
+# c3_f = 'x + y - z + 2'
+# c3_g = 'x**2 + y'
+# c3_h = 'z - y**2 - 1'
 
+# -- ----------------------------------------------------------------------- Caso Ejemplo -- #
+c1_f = 'x**3 + y**3 - z**3 - 129'
+c1_g = 'x**2 + y**2 - z**2 - 9.75'
+c1_h = 'x + y - z - 9.49'
+
+variables = [x, y, z]
+
+ejercicio_1 = newton_raphson(param_sis=[c1_f, c1_g, c1_h],
+                             param_ini=[4, 2, -3],
+                             param_error=1e-14)
+print(ejercicio_1)
