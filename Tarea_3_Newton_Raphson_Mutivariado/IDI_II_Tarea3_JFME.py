fbb4a4e149e47fb38f33d0464d2633062bf54143
IFFranciscoME <if.francisco.me@gmail.com>
2020-05-08 12:28:16 -0500

    rebase para inicio simple

--- Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
+++ Tarea_3_Newton_Raphson_Mutivariado/IDI_II_Tarea3_JFME.py
@@ -5,46 +5,35 @@
 # -- repositorio: https://github.com/IFFranciscoME/IDI_II_GIT
 # -- ------------------------------------------------------------------------------------ -- #
 
-import time
+"""
+Realice codigo en Python que, recibiendo un sistema de n ecuaciones no lineales
+fi(x1, x2, ... , xn) = 0, un valor inicial x=0 y una exactitud (error) dado E,
+use el metodo de Newton multivariable para encontrar (si existe) una solucion real del sistema.
+Asegurese que cuenta el numero de iteraciones realizadas.
+
+Use su codigo para encontrar (si existe) todas las soluciones reales de cada sistema con
+presición de 4 dígitos y exactitud de 10^-3. Puede utilizar medios gráficos para analizar la
+función previamente.
+
+En todos los casos indique el(los) valor(es) inicial(es) que utilizó y el número de
+iteraciones que fueron necesarias para alcanzar la respuesta:
+"""
+
 import sympy as sp
-from sympy.plotting import plot3d
-from sympy import symbols
 
-# -- NOTAS
-# -- -- Utilizar la inversa de sympy
+# declarar a x, y, z como variables simbolicas
+x, y, z = sp.symbols('x y z')
 
-# -- Caso 1
-# -- -- f(x,y) = x**2 + y - 1
-# -- -- g(x,y) = x - 2*y**2
-
-x, y, z = symbols('x y z')
+# -- ----------------------------------------------------------------------------- Caso 1 -- #
 c1_f = 'x**2 + y - 1'
 c1_g = 'x - 2*y**2'
 
-plot3d((c1_f, (x, -5, 5), (y, -5, 5)),
-       (c1_g, (x, -5, 5), (y, -5, 5)))
-
-# -- Caso 2
-# -- -- f(x,y) = x**2 - 10*x + y**2 + 5
-# -- -- g(x,y) = x*y**2 + x - 10*y + 8
-
+# -- ----------------------------------------------------------------------------- Caso 2 -- #
 c2_f = 'x**2 - 10*x + y**2 + 5'
 c2_g = 'x*y**2 + x - 10*y + 8'
 
-plot3d((c2_f, (x, -5, 5), (y, -5, 5)),
-       (c2_g, (x, -5, 5), (y, -5, 5)))
-
-# -- Caso 3
-# -- -- f(x,y) = x + y - z + 2
-# -- -- g(x,y) = x**2 + y
-# -- -- h(x,y) = z - y**2 - 1
-
+# -- ----------------------------------------------------------------------------- Caso 3 -- #
 c3_f = 'x + y - z + 2'
 c3_g = 'x**2 + y'
 c3_h = 'z - y**2 - 1'
 
-plot3d((c3_f, (x, -5, 5), (y, -5, 5), (z, -5, 5)),
-       (c3_g, (x, -5, 5), (y, -5, 5), (z, -5, 5)),
-       (c3_h, (x, -5, 5), (y, -5, 5), (z, -5, 5)))
-
-# -- Paso 1
